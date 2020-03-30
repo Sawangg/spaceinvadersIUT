6f9d69c6acd47cdf08eafb003b761db9a65c5ef6
Léo Mercier <le1112osee@gmail.com>
2020-03-30 17:32:33 +0200

    Amélioration de SpaceInvaders & Création de Vaisseau

--- spaceinvaders/src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ spaceinvaders/src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -4,22 +4,30 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
 public class SpaceInvadersTest {
 
-   @Test
-   public void test_AuDebut_JeuSpaceInvaderEstVide() {
-        SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
-        assertEquals("" + 
-        "...............\n" + 
-        "...............\n" +
-        "...............\n" + 
-        "...............\n" + 
-        "...............\n" + 
-        "...............\n" + 
-        "...............\n" + 
-        "...............\n" + 
-        "...............\n" + 
-        "...............\n" , spaceinvaders.toString());
-        }
+     @Test
+     public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+          SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+          spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+          assertEquals("" + 
+          "...............\n" + 
+          "...............\n" +
+          "...............\n" + 
+          "...............\n" + 
+          "...............\n" + 
+          "...............\n" + 
+          "...............\n" + 
+          "...............\n" + 
+          "...............\n" + 
+          ".......V.......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+     }
 
-   }
+     @Test(expected = HorsEspaceJeuException.class)
+	public void test_unNouveauVaisseauEstPositionneHorsEspaceJeuTropADroite_UneExceptionEstLevee() throws Exception {
+		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+		spaceinvaders.positionnerUnNouveauVaisseau(15,9);
+	}
+}
